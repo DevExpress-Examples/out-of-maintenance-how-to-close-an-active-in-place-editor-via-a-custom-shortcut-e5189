dce43e55dc6e733702c7a1fb82872c61fd697c56
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/E5189/DXApplication5/Data/DataHelper.vb
@@ -0,0 +1,48 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Linq
+
+Namespace DXApplication5
+    Public Class DataHelper
+        Private Shared rnd As New Random()
+
+        Private Shared Users() As String = { "Peter Dolan", "Ryan Fischer", "Andrew Miller", "Tom Hamlett", "Jerry Campbell", "Carl Lucas", "Mark Hamilton", "Steve Lee" }
+
+        Public Shared Sub FillResources(ByVal resources As BindingList(Of CustomResource), ByVal count As Integer)
+
+            Dim cnt As Integer = Math.Min(count, Users.Length)
+            For i As Integer = 1 To cnt
+                Dim resource As New CustomResource()
+                resource.Name = Users(i - 1)
+                resource.ResourceID = i
+                resources.Add(resource)
+            Next i
+        End Sub
+
+        Public Shared Sub GenerateEvents(ByVal eventList As BindingList(Of CustomAppointment), ByVal count As Integer, ByVal resources As BindingList(Of CustomResource))
+
+            For i As Integer = 0 To count - 1
+                Dim resource As CustomResource = resources(i)
+                Dim subjPrefix As String = resource.Name & "'s "
+                eventList.Add(CreateEvent(subjPrefix & "meeting", resource.ResourceID, 2, 5, Priority.High))
+                eventList.Add(CreateEvent(subjPrefix & "travel", resource.ResourceID, 3, 6, Priority.Low))
+                eventList.Add(CreateEvent(subjPrefix & "phone call", resource.ResourceID, 0, 10, Priority.Medium))
+            Next i
+        End Sub
+
+        Private Shared Function CreateEvent(ByVal subject As String, ByVal resourceId As Object, ByVal status As Integer, ByVal label As Integer, ByVal priority As Priority) As CustomAppointment
+            Dim apt As New CustomAppointment()
+            apt.Subject = subject
+            apt.OwnerId = resourceId
+            apt.Priority = priority
+            Dim rangeInMinutes As Integer = 60 * 24
+            apt.StartTime = Date.Today + TimeSpan.FromMinutes(rnd.Next(0, rangeInMinutes))
+            apt.EndTime = apt.StartTime.Add(TimeSpan.FromMinutes(rnd.Next(0, rangeInMinutes \ 4)))
+            apt.Status = status
+            apt.Label = label
+            Return apt
+        End Function
+    End Class
+End Namespace
---
+++ VB/E5189/DXApplication5/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Linq
+Imports DevExpress.XtraEditors
+Imports DevExpress.XtraScheduler
+Imports System.Diagnostics
+
+
+Namespace DXApplication5
+    Partial Public Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+
+            schedulerControl.Start = Date.Now
+            schedulerControl.GroupType = SchedulerGroupType.Resource
+
+            InitResources()
+            InitAppointments()
+        End Sub
+
+        Private resources As New BindingList(Of CustomResource)()
+        Private eventList As New BindingList(Of CustomAppointment)()
+        Private Sub InitResources()
+            Dim mappings As ResourceMappingInfo = Me.schedulerStorage.Resources.Mappings
+            mappings.Caption = "Name"
+            mappings.Id = "ResourceID"
+
+            DataHelper.FillResources(resources, 5)
+            Me.schedulerStorage.Resources.DataSource = resources
+        End Sub
+        Private Sub InitAppointments()
+            Dim mappings As AppointmentMappingInfo = Me.schedulerStorage.Appointments.Mappings
+            mappings.Start = "StartTime"
+            mappings.End = "EndTime"
+            mappings.Subject = "Subject"
+            mappings.AllDay = "AllDay"
+            mappings.Description = "Description"
+            mappings.Label = "Label"
+            mappings.Location = "Location"
+            mappings.RecurrenceInfo = "RecurrenceInfo"
+            mappings.ReminderInfo = "ReminderInfo"
+            mappings.ResourceId = "OwnerId"
+            mappings.Status = "Status"
+            mappings.Type = "EventType"
+
+
+            schedulerStorage.Appointments.CustomFieldMappings.Add(New AppointmentCustomFieldMapping("Priority", "Priority"))
+            DataHelper.GenerateEvents(eventList, schedulerStorage.Resources.Count, resources)
+            Me.schedulerStorage.Appointments.DataSource = eventList
+
+        End Sub
+        Private Sub Storage_AppointmentsChanged(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs) Handles schedulerStorage.AppointmentsChanged
+            Dim appointments As AppointmentBaseCollection = TryCast(e.Objects, AppointmentBaseCollection)
+            For Each appointment As Appointment In appointments
+                Debug.WriteLine(appointment.Subject)
+            Next appointment
+        End Sub
+        #Region "#InplaceEditorShowing"
+        Private Sub schedulerControl_InplaceEditorShowing(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.InplaceEditorEventArgs) Handles schedulerControl.InplaceEditorShowing
+            e.InplaceEditorEx = New MyInplaceEditor(e.SchedulerInplaceEditorEventArgs)
+        End Sub
+        #End Region ' #InplaceEditorShowing
+    End Class
+End Namespace
